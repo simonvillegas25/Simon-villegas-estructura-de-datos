595794f063511cfa6a671bed8a4063f68351c9f3
simonvillegas25 <145401790+simonvillegas25@users.noreply.github.com>
2026-08-28 07:50:49 -0500

    cambio nombre production

---
+++ ejercicio.java
@@ -0,0 +1,10 @@
+class MyApp extends statefulWidget {
+ widget build(buildContext context) {
+  nome: Scaffold(
+    appBar: AppBar(title: const Text('material app bar 1 ')
+    body: const Center(child: text ('hello world DEVELOP'))
+
+  )// scaffold
+  ); //MaterialApp
+ }
+}
